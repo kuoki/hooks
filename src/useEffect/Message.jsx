b1ff4d3a7c7c25ae66149fb0b7574ee9bdf58e44
Jesus Correa <jesusippog@gmail.com>
2023-12-18 14:16:52 -0500

    adding useeffect

---
+++ src/useEffect/Message.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+export const Message = () => {
+    const [coords, setCoords] = useState({x:0, y:0});
+
+    useEffect(() => {
+
+        const onMouseEvent = ( {x, y} ) => {
+            // const coords = {x, y};
+            setCoords({x, y})
+            
+        }
+
+        window.addEventListener('mousemove', onMouseEvent)
+        return () =>{
+            window.removeEventListener('mousemove', onMouseEvent)
+        }
+    }, [])
+    return (
+        <>
+            <h3>El usuario ya existe</h3>
+            { JSON.stringify(coords)}
+        </>
+    )
+}
--- src/useEffect/SimpleForm.jsx
+++ src/useEffect/SimpleForm.jsx
@@ -1,31 +1,58 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { Message } from "./Message";
 
 export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: "lear",
+    email: "le@r.com",
+  });
 
-    const [formState, setFormState] = useState({
-        username: "lear",
-        email: "le@r.com"
-    })
+  const { username, email } = formState;
 
-    const { username, email } = formState;
-    
-    const onInputChange = ({target})=> {
-        const { name, value} = target
-        setFormState({
-            ...formState, 
-            [ name ] : value
-        })
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+  useEffect(() => {
+    // console.log("useEffect Go");
+  }, []);
+
+  useEffect(() => {
+    // console.log("useEffect cambio");
+  }, [formState]);
+
+  useEffect(() => {
+    // console.log("email cambio");
+  }, [email]);
+
+
+
+  return (
+    <>
+      <h1>Simple form</h1>
+      <hr />
+
+      <input
+        type="text"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+      <input
+        type="text"
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+
+    {
+        username === "lear" && <Message />
     }
-
-    useEffect( () => 
-    console.log('useEffect Go'));
-    return (
-        <>
-        <h1>Simple form</h1>
-        <hr />
-
-        <input type="text" placeholder="Username" name="username" value={username} onChange={ onInputChange}/>
-        <input type="text" placeholder="Email" name="email" value={email} onChange={ onInputChange}/>
-        </>
-    )
-}
+    </>
+  );
+};
