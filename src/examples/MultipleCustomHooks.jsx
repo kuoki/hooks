3d19be38136b7175c9b975d80d8fb332eac3846c
Jesús Correa <56005821+kuoki@users.noreply.github.com>
2023-12-19 23:23:47 -0500

    updating hooks

--- src/examples/MultipleCustomHooks.jsx
+++ src/examples/MultipleCustomHooks.jsx
@@ -6,11 +6,31 @@ export const MultipleCustomHooks = () => {
 
     const { data, hasError, isLoading } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
 
-    console.log({data, hasError, isLoading })
+    const {author, quote} = !!data && data[0]
+    // console.log({data, hasError, isLoading })
+
     return (
         <>
             <h1>BreakingBad Quotes</h1>
             <hr />
+
+            {
+                isLoading
+                    ? (
+                        <div> Loading ...</div>
+                    )
+             : (
+                <blockquote>
+                <p>{quote}</p>
+                <footer> {author}</footer>
+            </blockquote>
+            )
+             }
+            
+             <button>
+                Next quote
+             </button>
+         
         </>
     )
 }
